9be22d3d0d593502268d3b167d695df28752fb6c
Sergey Danilov <sddanilov_1@edu.hse.ru>
2022-04-07 21:59:29 +0300

    refactor product card class

--- lib/services/repository_gs1.dart
+++ lib/services/repository_gs1.dart
@@ -1,5 +1,7 @@
+import 'package:html/dom.dart';
 import 'package:http/http.dart' as http;
 import 'package:html/parser.dart';
+import 'package:http/http.dart';
 import 'dart:async';
 import '../models/product_card.dart';
 
@@ -19,47 +21,51 @@ class RepositoryGS1 {
   static const String gs1Address = "https://srs.gs1ru.org/id/gtin";
 
   Future<ProductCard?> getInfoByGTIN(String gtinID) async {
-    var searchAdress = "$gs1Address/$gtinID";
+    var res = await getSearchPage(gtinID);
+    if (res.statusCode != 200) {
+      throw Exception("Item $gtinID was not found");
+    }
+    var document = parse(res.body);
+    var manufacturer = getManufacturer(document);
+    var short_description = getShortDescription(document);
+    var brand = getProductName(document);
+    var card = ProductCard(
+        gtin: gtinID,
+        manufacturer: manufacturer,
+        productName: brand,
+        shortDescription: short_description);
+    return card;
+  }
+
+  Future<Response> getSearchPage(String gtin) async {
+    var searchAdress = "$gs1Address/$gtin";
     print("get product info from $searchAdress");
     var res = await http.get(Uri.parse(searchAdress));
-    if (res.statusCode == 200) {
-      var document = parse(res.body);
-      var manufacturer = document
-          .getElementsByClassName("product-card__header-company")[0]
-          .innerHtml
-          .trim();
+    return res;
+  }
 
-      var short_description = document
-          .getElementsByClassName("product-card__header-product-name")[0]
-          .innerHtml
-          .trim();
+  String getManufacturer(Document document) {
+    return document
+        .getElementsByClassName("product-card__header-company")[0]
+        .innerHtml
+        .trim();
+  }
 
-      var weight = document
-          .getElementsByTagName("table")[1]
-          .getElementsByTagName("tbody")[0]
-          .getElementsByTagName("tr")[3]
-          .getElementsByTagName("td")[0]
-          .getElementsByClassName("text2")[0]
-          .innerHtml
-          .trim();
+  String getShortDescription(Document document) {
+    return document
+        .getElementsByClassName("product-card__header-product-name")[0]
+        .innerHtml
+        .trim();
+  }
 
-      var brand = document
-          .getElementsByTagName("table")[1]
-          .getElementsByTagName("tbody")[0]
-          .getElementsByTagName("tr")[2]
-          .getElementsByTagName("td")[0]
-          .getElementsByClassName("text2")[0]
-          .innerHtml
-          .trim();
-
-      var card = ProductCard(
-          gtin: gtinID,
-          manufacturer: manufacturer,
-          productName: brand,
-          shortDescription: short_description);
-      return card;
-    } else {
-      return null;
-    }
+  String getProductName(Document document) {
+    return document
+        .getElementsByTagName("table")[1]
+        .getElementsByTagName("tbody")[0]
+        .getElementsByTagName("tr")[2]
+        .getElementsByTagName("td")[0]
+        .getElementsByClassName("text2")[0]
+        .innerHtml
+        .trim();
   }
 }
